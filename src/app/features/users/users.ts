47eabc657dfd5e499adb7fceb56d3741c003429b
jose manuel <jsipacc@miumg.edu.gt>
2026-03-06 20:44:45 -0600

    actualizaciones del crud, no separados pero funcionan

--- src/app/features/users/users.ts
+++ src/app/features/users/users.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { ApiService } from '../../core/api.service';
 import { User } from '../../models/user';
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -9,45 +10,63 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './users.html',
-  styleUrl: './users.css'
+  styleUrls: ['./users.css']
 })
 export class Users implements OnInit {
 
   users: User[] = [];
+  showForm: boolean = false;
+  newUser: User = { firstName: '', lastName: '', age: 0, email: '' };
 
-  user: User = {
-    firstName:'',
-    lastName:'',
-    age:0,
-    email:''
-  };
+  constructor(
+    private api: ApiService, 
+    private router: Router, 
+    private cd: ChangeDetectorRef
+  ){}
 
-  constructor(private api:ApiService){}
-
-  ngOnInit(){
-    this.loadUsers();
+  ngOnInit(): void {
+    this.loadUsers(); // carga automática al iniciar
   }
 
-  loadUsers(){
-    this.api.getUsers().subscribe(data=>{
-      this.users = data;
+  loadUsers(): void {
+    this.api.getUsers().subscribe({
+      next: data => {
+        this.users = data; // asigna datos
+        this.cd.detectChanges(); // fuerza actualización de la tabla
+      },
+      error: err => console.error('Error cargando usuarios', err)
     });
   }
 
-  saveUser(){
-
-    this.api.createUser(this.user).subscribe(()=>{
-      this.loadUsers();
-    });
-
+  deleteUser(id: number): void {
+    this.api.deleteUser(id).subscribe(() => this.loadUsers());
   }
 
-  deleteUser(id:number){
-
-    this.api.deleteUser(id).subscribe(()=>{
-      this.loadUsers();
-    });
-
+  editUser(u: User): void {
+    this.router.navigate(['/users/edit', u.id]); // redirige al formulario de edición
   }
 
+  addNew(): void {
+    this.showForm = true;
+  }
+
+  saveNewUser(): void {
+    this.api.createUser(this.newUser).subscribe({
+      next: () => {
+        this.loadUsers();
+        this.showForm = false;
+        this.resetNewUser();
+      },
+      error: err => console.error('Error creando usuario', err)
+    });
+  }
+
+  cancelAdd(): void {
+    this.showForm = false;
+    this.resetNewUser();
+  }
+
+  private resetNewUser(): void {
+    this.newUser = { firstName: '', lastName: '', age: 0, email: '' };
+  }
 }
